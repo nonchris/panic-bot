94e8a24fa7487847486a0f05ded5ebb3f0c50101
ceron21 <mbp@chris-ge.de>
2020-05-22 00:09:09 +0200

    a working version of the join scanner

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 #bot.py
 import os
 import random
-from dotenv import load_dotenv #for work with .env files
+from dotenv import load_dotenv #for work with .env files (credentials)
+import config #for server configs
 
 import discord
 from discord.ext import commands
@@ -11,6 +12,8 @@ from discord.ext.commands import Bot
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN') #reading in the token from .env file
 #GUILD = os.getenv('DISCORD_GUILD')
+
+server_channels = {} # Server channel cache
 
 #setting prefix and defining bot
 bot = commands.Bot(command_prefix='!')
@@ -23,7 +26,7 @@ async def on_ready():
 	guild = discord.utils.get(bot.guilds)#, name=GUILD)
 	print(
 		f'The bot is connected to the following guild:\n'
-		f'{guild.name}(id: {guild.id})'
+		f'{guild.name} (id: {guild.id})'
 	)
 
 ##__Responder__##
@@ -84,14 +87,95 @@ async def create_channel(ctx, channel_name='generated-voice-channel'):
 async def on_command_error(ctx, error):
 	if isinstance(error, commands.errors.CheckFailure):
 			await ctx.send('You can\'t do that. Pleases ask an Admin')
+##Not working
+# @bot.command(name='join', pass_context=True)
+# async def join(ctx):
+# 	channel = ctx.message.author.voice.voice_channel
+# 	await client.join_channel(channel)
 
-@bot.command(name='join', pass_context=True)
-async def join(ctx):
-	channel = ctx.message.author.voice.voice_channel
-	await client.join_channel(channel)
+
+### CHECK FOR CHANNEL JOIN ###
+
+def find_channel(server, refresh = False):
+    """
+    Find and return the channel to log the voice events to.
+    
+    :param server: The server to find the channel for.
+    :param refresh: Whether to refresh the channel cache for this server.
+    """
+    if not refresh and server in server_channels:
+        return server_channels[server]
+        
+    for channel in client.get_all_channels():
+        if channel.server == server and channel.name == config.CHANNEL_NAME:
+            print("%s: refreshed destination log channel" % server)
+            server_channels[server] = channel
+            return channel
+            
+    return None
 
 
-##
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    #if before.channel is None and after.channel is not None:
+    if before.channel is None and after.channel is not None:
+        #print("there is something")
+        print(after.channel)
+        print(member.guild.system_channel)
+        if after.channel.id == 712247842995175426:
+            print("panic!!!")
+            channel = "Allgemein"
+            await member.guild.system_channel.send("Alarm!")
+            
+
+# @client.event
+# async def on_voice_state_update(member_before, member_after):
+#     """
+#     Called when the voice state of a member on a server changes.
+    
+#     :param member_before: The state of the member before the change.
+#     :param member_after: The state of the member after the change.
+#     """
+#     server = member_after.server
+#     channel = find_channel(server)
+    
+#     voice_channel_before = member_before.voice_channel
+#     voice_channel_after = member_after.voice_channel
+    
+#     print("something happend")
+#     if voice_channel_before == voice_channel_after:
+#         # No change
+#         return
+    
+#     if voice_channel_before == None:
+#         # The member was not on a voice channel before the change
+#         msg = "%s joined voice channel _%s_" % (member_after.mention, voice_channel_after.name)
+#     else:
+#         # The member was on a voice channel before the change
+#         if voice_channel_after == None:
+#             # The member is no longer on a voice channel after the change
+#             msg = "%s left voice channel _%s_" % (member_after.mention, voice_channel_before.name)
+#         else:
+#             # The member is still on a voice channel after the change
+#             msg = "%s switched from voice channel _%s_ to _%s_" % (member_after.mention, voice_channel_before.name, voice_channel_after.name)
+    
+#     # Try to log the voice event to the channel
+#     try:
+#         await client.send_message(channel, msg)
+#     except:
+#         # No message could be sent to the channel; force refresh the channel cache and try again
+#         channel = find_channel(server, refresh = True)
+#         if channel == None:
+#             # The channel could not be found
+#             print("Error: channel #%s does not exist on server %s." % (config.CHANNEL_NAME, server))
+#         else:
+#             # Try sending a message again
+#             try:
+#                 await client.send_message(channel, msg)
+#             except discord.DiscordException as exception:
+#                 # Print the exception
+#                 print("Error: no message could be sent to channel #%s on server %s. Exception: %s" % (config.CHANNEL_NAME, server, exception))
 
 
 bot.run(TOKEN)
