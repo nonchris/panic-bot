a2583434ee428772007ba25d4443bd59e65f29cc
ceron21 <mbp@chris-ge.de>
2020-05-23 12:41:26 +0200

    updated gitignore

--- config.py
+++
@@ -1,3 +0,0 @@
-# The bot token
-#DISCORD_TOKEN='Njk0NjM3MTU0NTgzODM4ODAx.XsJYng.2kXZQP3HTIBr_-rD6mZPmBtm-vc' #live version
-DISCORD_TOKEN='NzEzNjc5NzEyODYxMjkwNTU2.XsjoMA.YJpqMytw6FKsw8br0NkuOHznnIA' #development
